c50cf7048b2142e88215aaf758712a9429d6b6d2
Jimmy <jimmylu10@gmail.com>
2022-05-27 12:08:31 -0700

    yes

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -9,13 +9,14 @@ class Title extends Phaser.Scene {
 
         // load JSON (dialog)
         this.load.json('dialog', 'json/dialog.json');
-
+        this.load.json('macbeth', 'json/macbeth.json');
         // load images
         this.load.image('dialogbox', 'img/dialogbox.png');
         this.load.image('homer', 'img/homer.png');
         this.load.image('minerva', 'img/minerva.png');
         this.load.image('jove', 'img/jove.png');
         this.load.image('neptune', 'img/neptune.png');
+        this.load.image('witch', 'img/witch.png');
 
         // load bitmap font
         this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml');
